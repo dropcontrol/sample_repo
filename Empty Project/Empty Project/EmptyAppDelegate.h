4996865ef4dc0c17d04ceff2590e97f9d30df80c
hiroshi yamato <yamato@allianceport.jp>
2013-07-05 16:39:11 +0900

    first commit

---
+++ Empty Project/Empty Project/EmptyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EmptyAppDelegate.h
+//  Empty Project
+//
+//  Created by hiroshi yamato on 7/5/13.
+//  Copyright (c) 2013 Alliance Port, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmptyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Empty Project/Empty Project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Empty Project
+//
+//  Created by hiroshi yamato on 7/5/13.
+//  Copyright (c) 2013 Alliance Port, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EmptyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EmptyAppDelegate class]));
+    }
+}
